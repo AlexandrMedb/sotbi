6a039f47d05f082751649f956b86604f0941eda1
Alexandr <torasov.alexandr@gmail.com>
2021-10-25 15:03:06 +0300

    doneTask4

---
+++ task4/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./App.css";
+
+import "./App.css";
+import { Task4 } from "./components/task4";
+
+function App() {
+  return <Task4 />;
+}
+
+export default App;
